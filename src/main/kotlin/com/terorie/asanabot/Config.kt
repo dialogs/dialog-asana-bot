ba718aefafb8cf51181d51adbb989549e983a0a2
Richard Patel <terorie@alphakevin.club>
2018-10-23 22:33:32 +0200

    Load tokens from properties file

--- src/main/kotlin/com/terorie/asanabot/Config.kt
+++ src/main/kotlin/com/terorie/asanabot/Config.kt
@@ -1,5 +1,24 @@
 package com.terorie.asanabot
 
-const val BOT_TOKEN = "-"
-const val ASANA_PERSONAL_TOKEN = "-"
-const val ASANA_TEAM_ID = "-"
+import java.io.FileInputStream
+import java.lang.IllegalArgumentException
+import java.util.*
+
+object Config {
+    val BOT_TOKEN: String
+    val ASANA_PERSONAL_TOKEN: String
+    val ASANA_TEAM_ID: String
+
+    init {
+        val props = Properties()
+        FileInputStream("config.properties").use {
+            props.load(it)
+        }
+        BOT_TOKEN = props.getProperty("bot_token")
+            ?: throw IllegalArgumentException("bot_token not set")
+        ASANA_PERSONAL_TOKEN = props.getProperty("asana_personal_token")
+            ?: throw IllegalArgumentException("asana_personal_token not set")
+        ASANA_TEAM_ID = props.getProperty("asana_team_id")
+            ?: throw IllegalArgumentException("asana_team_id not set")
+    }
+}
